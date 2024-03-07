b8a2773a754ef5d0ebca4a8d5231bf7e43eb9459
Venerablevivek <venerablevivekch@gmail.com>
2024-03-07 22:31:55 +0530

    added code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react([
-    'src/index.css',
-    'src/main.jsx', // Update the entry module here
-])],
+  plugins: [react()],
 })
