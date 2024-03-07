f448125cac131bd88804bae65c050994b50123a3
Venerablevivek <venerablevivekch@gmail.com>
2024-03-07 22:16:11 +0530

    Added vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react([
+    'resources/css/app.css',
+    'resources/js/main.jsx', // Update the entry module here
+])],
 })
